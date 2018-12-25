27742a6e81adde6495ff8f1f92bfed22866368d8
caiwen1 <caiwen_5050@163.com>
2018-12-25 18:39:24 +0800

    除去判断内容~~~~

--- cloud-provider-dept-8001/src/main/java/com/fangxie365/provider/controller/DeptController.java
+++ cloud-provider-dept-8001/src/main/java/com/fangxie365/provider/controller/DeptController.java
@@ -15,9 +15,6 @@ public class DeptController {
     @GetMapping(value = "/dept/get/{id}")
     public Dept get(@PathVariable("id") Long id)
     {
-        if(id!=11){
-            System.out.println(11);
-        }
         return deptService.findById(id);
     }
 }
